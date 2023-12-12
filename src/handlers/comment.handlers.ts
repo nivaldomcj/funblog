4719b73c33d1118b16347bf61f9f33f687aa14b3
Nivaldo Carvalho <nivaldo_j@outlook.com>
2023-12-12 18:49:45 -0300

    removed unused files

--- src/handlers/comment.handlers.ts
+++
@@ -1,9 +0,0 @@
-export const listComments = () => 'TODO';
-
-export const createComment = () => 'TODO';
-
-export const getComment = () => 'TODO';
-
-export const updateComment = () => 'TODO';
-
-export const deleteComment = () => 'TODO';
--- src/handlers/post.handlers.ts
+++
@@ -1,9 +0,0 @@
-export const listPosts = () => 'TODO';
-
-export const getPost = () => 'TODO';
-
-export const createPost = () => 'TODO';
-
-export const updatePost = () => 'TODO';
-
-export const deletePost = () => 'TODO';
